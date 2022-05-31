2838c063b2d08b79607030345e7c60908197f092
Mario3999 <mario.malitesta@gmail.com>
2022-05-31 18:40:09 +0200

    fixings

--- src/components/RecipeDetails/RecipeDetails.js
+++ src/components/RecipeDetails/RecipeDetails.js
@@ -10,7 +10,7 @@ const RecipeDetails = () => {
 
   const pathname = unstrippedPathname.substring(1, unstrippedPathname.length)
 
-  const [titles, setTitles] = useState([]);
+  const [titles, setTitles] = useState();
 
   useEffect(()=>{fetchApi();},[]);
   
@@ -18,17 +18,21 @@ const RecipeDetails = () => {
     try{
       const id = pathname
       const response = await axios.get(`https://api.spoonacular.com/recipes/${id}/information?apiKey=7fad6a4779ae494389675988d67a0280`)
+      // console.log(response)
       setTitles(response)
     }
     catch(error){
       alert(error)
     }
   }
+  if(!titles){
+    return <div>Loading</div>
+  }
   return (
     <div className={styles['container']}>
-      <h1>{titles.data.title}</h1>
-      <img src={titles.data.image} alt="Immagine" />
-      <div className={styles['summary']}>{titles.data.summary}</div>
+       <h1>{titles.data.title}</h1>
+       <img src={titles.data.image} alt="Immagine" />
+       <div className={styles['summary']}>{titles.data.summary}</div>
     </div>
   )
 }
